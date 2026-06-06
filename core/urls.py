170112b5ee5e123a96de4d6f4087f866e8973efa
Vitaliy <movcan77@gmail.com>
2026-06-06 10:54:45 +0300

    Add subjects crud

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import RegistrationView
+from .views import (
+    RegistrationView,
+    AddSubjectView,
+    DashboardView,
+    DeleteSubjectView,
+    SubjectDetailView
+)
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name="register")
+    path('register/', RegistrationView.as_view(), name="register"),
+    path('', DashboardView.as_view(), name="dashboard"),
+    path('subjects/add/', AddSubjectView.as_view(), name="add_subject"),
+    path('subjects/<int:pk>/delete', DeleteSubjectView.as_view(), name="delete_subject"),
+    path('subjects/<int:pk>/', SubjectDetailView.as_view(), name="subject_detail")
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, DetailView, DeleteView
 from django.utils import timezone
 from .forms import RegistrationForm
 from .models import Subject, StudySession, Streak
@@ -14,7 +14,7 @@ class RegistrationView(CreateView):
     success_url = reverse_lazy("login")
 
 class DashboardView(LoginRequiredMixin, TemplateView):
-    template_name = "dashboard.html"
+    template_name = "tracker/dashboard.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -41,4 +41,41 @@ class DashboardView(LoginRequiredMixin, TemplateView):
             s.duration for s in sessions
         ])
         return context
+
+class AddSubjectView(LoginRequiredMixin, CreateView):
+    model = Subject
+    template_name = "tracker/add_subject.html"
+    fields = ["name"]
+    success_url = reverse_lazy("dashboard")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class SubjectDetailView(LoginRequiredMixin, DetailView):
+    model = Subject
+    template_name = "tracker/subject_detail.html"
+    context_object_name = "subject"
+
+    def get_queryset(self):
+        return Subject.objects.filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        sessions = StudySession.objects.filter(
+            subject=self.object
+        )
+        total_time = sum(
+            s.duration for s in sessions
+        )
+        context["sessions"] = sessions
+        context["total_time"] = total_time
+        return context
+
+class DeleteSubjectView(LoginRequiredMixin, DeleteView):
+    model = Subject
+    success_url = reverse_lazy("dashboard")
+
+    def get_queryset(self):
+        return Subject.objects.filter(user=self.request.user)
 
